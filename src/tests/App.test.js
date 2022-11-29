e6e1c57900b2e9a4206d4349c517bb9ffbe4da1d
Giulliana Rangel <giulliana.rangel@gmail.com>
2022-11-29 20:17:32 -0300

    feat: add re1 - tests 90% coverage

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Req1. atingir 90% de cobertura em todo o teste', () => {
+  test('Testando pagina de login', async () => {
+    // const { history } =
+    render(<App />);
+    const email = screen.getByPlaceholderText(/Digite seu email/i);
+    expect(email).toBeInTheDocument();
+
+    const password = screen.getByTestId('password-input');
+    userEvent.type(password, '1234567');
+
+    const buttonLogin = screen.getByRole('button', { name: /enter/i });
+    expect(buttonLogin).toBeInTheDocument();
+    expect(buttonLogin).toBeDisabled();
+
+    userEvent.type(email, 'test@gmail.com');
+
+    expect(buttonLogin).not.toBeDisabled();
+    userEvent.click(buttonLogin);
+    // const { pathname } = history.location;
+    // expect(pathname).toBe('/');
+  });
 });
