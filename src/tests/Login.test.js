8cdc535127f974cbf7f1b36c1e028071aa27aa58
Giulliana Rangel <giulliana.rangel@gmail.com>
2022-11-30 15:16:42 -0300

    fix: req1 : cover localStorage

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from './helpers/renderWithRouter';
+import { getFromLocal } from '../services/storage';
 
 describe('Req1. atingir 90% de cobertura em todo o teste', () => {
   test('Testando pagina de login', async () => {
-    // const { history } =
-    render(<App />);
+    const { history } = renderWithRouter(<App />);
     const email = screen.getByPlaceholderText(/Digite seu email/i);
     expect(email).toBeInTheDocument();
 
@@ -21,10 +22,14 @@ describe('Req1. atingir 90% de cobertura em todo o teste', () => {
 
     expect(buttonLogin).not.toBeDisabled();
     userEvent.click(buttonLogin);
-    // const { pathname } = history.location;
-    // expect(pathname).toBe('/');
 
-    // const testLocalStorage = JSON.parse(localStorage.getItem('key'));
-    // expect(testLocalStorage.email).toBe('test@gmail.com');
+    // await waitForElementToBeRemoved(buttonLogin, { timeout: 2000 });
+    // expect(history.location.pathname).toBe('/meals');
+
+    const testLocalStorage = getFromLocal('user');
+    expect(testLocalStorage.email).toBe('test@gmail.com');
+
+    const testLocalStorage1 = getFromLocal('Karol');
+    expect(testLocalStorage1).toBe('Não foi encontrada a chave Karol');
   });
 });
