a77c956981cb4d76aa61c2cc154b8289ac0bbcfa
RaphaelRighetti <raphaelrighetti@hotmail.com>
2022-11-30 20:43:33 -0300

    req 10, 11, 12, 13, 14, 15, done :rocket:

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
 
-export default function Drinks() {
+export default function Drinks({ history }) {
+  const { recipes } = useContext(RecipesContext);
+  const maxRecipes = 12;
   return (
     <div>
-      <Header pageTitle="Drinks" searchBtn />
+      <Header
+        pageTitle="Drinks"
+        searchBtn
+        url="https://www.thecocktaildb.com/api/json/v1/1/"
+        history={ history }
+      />
+      {recipes.drinks?.length > 0 && recipes.drinks.filter((_e, i) => i < maxRecipes)
+        .map((element, index) => (
+          <div key={ element.idDrink } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ element.strDrinkThumb }
+              alt={ element.strDrink }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>
+              { element.strDrink }
+            </p>
+          </div>
+        ))}
     </div>
   );
 }
+
+Drinks.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
 
-export default function Meals() {
+export default function Meals({ history }) {
+  const { recipes } = useContext(RecipesContext);
+  const maxRecipes = 12;
   return (
     <div>
-      <Header pageTitle="Meals" searchBtn />
+      <Header
+        pageTitle="Meals"
+        searchBtn
+        url="https://www.themealdb.com/api/json/v1/1/"
+        history={ history }
+      />
+      {recipes.meals?.length > 0 && recipes.meals.filter((_e, i) => i < maxRecipes)
+        .map((element, index) => (
+          <div key={ element.idMeal } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ element.strMealThumb }
+              alt={ element.strMeal }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>
+              { element.strMeal }
+            </p>
+          </div>
+        ))}
     </div>
   );
 }
+
+Meals.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
