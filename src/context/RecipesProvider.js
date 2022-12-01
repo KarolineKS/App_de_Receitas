396516ce0f179fb2f6237944c4f7e61490e60ed2
RaphaelRighetti <raphaelrighetti@hotmail.com>
2022-12-01 19:46:44 -0300

    reqs 19, 20, 21, 22, 23, done :rocket:

--- src/context/RecipesProvider.js
+++ src/context/RecipesProvider.js
@@ -1,14 +1,32 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import RecipesContext from './RecipesContext';
 
 export default function RecipesProvider({ children }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [radioSearch, setRadioSearch] = useState('i');
-  const [recipes, setRecipes] = useState({
-    meals: [],
-    drinks: [],
-  });
+  const [recipes, setRecipes] = useState({ meals: [] });
+  const [drinks, setDrinks] = useState({ drinks: [] });
+  const [mealsCategories, setMealsCategories] = useState({ meals: [] });
+  const [drinksCategories, setDrinksCategories] = useState({ drinks: [] });
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+  const fetchRecipes = async () => {
+    const maxRecipes = 12;
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    data.meals = data.meals.filter((_e, i) => i < maxRecipes);
+    setRecipes(data);
+    console.log(data);
+  };
+
+  const fetchDrinks = async () => {
+    const maxRecipes = 12;
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    data.drinks = data.drinks.filter((_e, i) => i < maxRecipes);
+    setDrinks(data);
+  };
 
   const value = useMemo(() => ({
     email,
@@ -19,7 +37,31 @@ export default function RecipesProvider({ children }) {
     setRadioSearch,
     recipes,
     setRecipes,
-  }), [email, password, radioSearch, recipes]);
+    drinks,
+    setDrinks,
+    mealsCategories,
+    drinksCategories,
+    fetchRecipes,
+    fetchDrinks,
+    selectedCategory,
+    setSelectedCategory,
+  }), [email, password, radioSearch, recipes,
+    drinks, mealsCategories, drinksCategories, selectedCategory]);
+
+  const fetchCategories = async (url, setFunc, key) => {
+    const maxCategories = 5;
+    const response = await fetch(url);
+    const data = await response.json();
+    data[key] = data[key].filter((_e, i) => i < maxCategories);
+    setFunc(data);
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+    fetchDrinks();
+    fetchCategories('https://www.themealdb.com/api/json/v1/1/list.php?c=list', setMealsCategories, 'meals');
+    fetchCategories('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list', setDrinksCategories, 'drinks');
+  }, []);
 
   return (
     <RecipesContext.Provider value={ value }>
