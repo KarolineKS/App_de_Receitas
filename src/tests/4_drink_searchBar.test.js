2bee99761cc6836353e65db9f64f7fc633c1e328
Giulliana Rangel <giulliana.rangel@gmail.com>
2022-12-06 08:13:52 -0300

    fix: coverage 88%

--- src/tests/4_drink_searchBar.test.js
+++ src/tests/4_drink_searchBar.test.js
@@ -3,13 +3,15 @@ import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
+import { DRINKS_MOCK } from './helpers/mock_recipes';
 
 const searchInputDti = 'search-input';
 const firstLetterDti = 'first-letter-search-radio';
 const nameDti = 'name-search-radio';
 const ingredientsDti = 'ingredient-search-radio';
-const btnDti = 'exec-search-btn';
-const btnTopDti = 'search-top-btn';
+const btnSearchDti = 'exec-search-btn';
+const btnIconDti = 'search-top-btn';
+const alertMessage = 'Sorry, we haven\'t found any recipes for these filters.';
 
 describe('Testando componente Drinks', () => {
   test('Verifique a busca por Drinks', async () => {
@@ -48,48 +50,71 @@ describe('Testando componente Drinks', () => {
     userEvent.click(mealBtnIcon);
     expect(history.location.pathname).toBe('/meals');
   });
+
   test('O campo de pesquisa para os Drinks', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(DRINKS_MOCK),
+    });
     const { history } = renderWithRouter(<App />);
     act(() => {
       history.push('/drinks');
     });
     await screen.findByRole('heading', { name: /Drinks/i });
 
-    const searchInput = screen.queryByTestId(searchInputDti);
-    const name = screen.getByTestId(nameDti);
-    const btnTop = screen.getByTestId(btnTopDti);
-    const btn = await screen.findByTestId(btnDti);
-    const ingredient = screen.findByTestId(ingredientsDti);
+    const searchInput = screen.findByTestId(searchInputDti);
+    const btnSearch = screen.findByTestId(btnSearchDti);
+    const iconSearch = screen.findByTestId(btnIconDti);
     const firstLetter = screen.findByTestId(firstLetterDti);
+    const ingredient = screen.findByTestId(ingredientsDti);
 
-    userEvent.click(btnTop);
-    waitFor(() => {
-      expect(searchInput).toBeVisible();
+    userEvent.click(await iconSearch);
+
+    expect(await searchInput).toBeVisible();
+
+    userEvent.type(await searchInput, 'a');
+    userEvent.click(await firstLetter);
+    userEvent.click(await btnSearch);
+
+    expect(await screen.findByText('A True Amaretto Sour'));
+
+    userEvent.type(searchInput, '');
+    userEvent.type(searchInput, 'lemon');
+    userEvent.click(await ingredient);
+
+    expect(await screen.findByText('A True Amaretto Sour'));
+
+    const name = screen.findByTestId(nameDti);
+    userEvent.type(searchInput, '');
+    userEvent.type(searchInput, 'boston');
+    userEvent.click(await name);
+
+    expect(await screen.findByText(/Boston Sour/i));
+  });
+
+  test('Testando mensagem de erro, linhas 31 e 32 SearchBar', async () => {
+    global.alert = jest.fn();
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/drinks');
     });
-    userEvent.type(searchInput, 'Cuba Libre');
-    userEvent.click(name);
-    userEvent.click(btn);
 
-    waitFor(() => {
-      expect(screen.getByText('Cuba Libre')).toBeVisible();
-    });
+    await screen.findByRole('heading', { name: /Drinks/i });
 
-    waitFor(() => {
-      userEvent.clear(searchInput);
-      userEvent.type(searchInput, 'Lemon');
-      userEvent.click(ingredient);
+    const searchInput = screen.findByTestId(searchInputDti);
+    const btnSearch = screen.findByTestId(btnSearchDti);
+    const iconSearch = screen.findByTestId(btnIconDti);
 
-      expect(screen.getByText('California Lemonade')).toBeVisible();
-    });
+    userEvent.click(await iconSearch);
 
-    userEvent.click(btn);
+    expect(await searchInput).toBeVisible();
 
-    userEvent.type(searchInput, 'l');
+    userEvent.type(searchInput, '');
+    userEvent.click(await btnSearch);
 
-    waitFor(() => {
-      userEvent.click(firstLetter);
-      userEvent.click(btn);
-      expect(screen.getByText('Limeade')).toBeVisible();
+    await waitFor(async () => {
+      expect(global.alert).toBeCalledWith(alertMessage);
+      expect(global.alert).toHaveBeenCalledTimes(1);
     });
   });
 });
