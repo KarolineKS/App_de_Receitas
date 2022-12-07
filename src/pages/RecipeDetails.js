a974b87e66b9c6b0dd402a7fe119ae32212c5276
Saulo Vasconcelos <saulovsf@gmail.com>
2022-12-06 21:31:50 -0400

    Finaliza req 25, 26, 28, 31, 32, 33, 34

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import copy from 'clipboard-copy';
+import { getFromLocal, saveOnStorage } from '../services/storage';
 
-function RecipeDetails({ match }) {
+function RecipeDetails({ match, history, location }) {
   const [detailsRecipes, setDetailsRecipe] = useState({
     drinks: [],
     meals: [],
   });
 
   const [ingredientes, setIngredientes] = useState([]);
+  const [pound, setPound] = useState([]);
+  const [showCopy, setShowCopy] = useState(false);
 
   const type = match.path.split('/')[1];
   const FetchUrl = async (url) => {
@@ -19,16 +23,39 @@ function RecipeDetails({ match }) {
     ));
     const newRecipes = recipes.map((e) => (
       data[type][0][e]
-    )).filter((a) => a !== '');
+    )).filter((a) => a !== '' && a !== null);
+    const ingredientPounds = Object.keys(data[type][0]).filter((e) => (
+      e.includes('strMeasure')
+    ));
+    const newPound = ingredientPounds.map((e) => (
+      data[type][0][e]
+    )).filter((a) => a !== '' && a !== null);
     setIngredientes(newRecipes);
-    console.log('newrecipes', newRecipes);
+    setPound(newPound);
   };
 
+  const { params: { id } } = match;
+
   useEffect(() => {
-    const { params: { id } } = match;
     const url = type === 'meals' ? `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}` : `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
     FetchUrl(url);
   }, []);
+
+  const saveFavorites = (recipe) => {
+    const obj = {
+      id: recipe.idMeal || recipe.idDrink,
+      type: type === 'meals' ? 'meal' : 'drink',
+      nationality: recipe.strArea || '',
+      category: recipe.strCategory,
+      alcoholicOrNot: recipe.strAlcoholic || '',
+      name: recipe.strMeal || recipe.strDrink,
+      image: recipe.strMealThumb || recipe.strDrinkThumb,
+    };
+
+    const local = getFromLocal('favoriteRecipes');
+    const newLocal = typeof local === 'string' ? [obj] : [...local, obj];
+    saveOnStorage('favoriteRecipes', newLocal);
+  };
 
   return (
     <div>
@@ -37,7 +64,7 @@ function RecipeDetails({ match }) {
 
           <div key={ recipe.idMeal || recipe.idDrink }>
             <h1 data-testid="recipe-title">
-              {recipe.strMeal || recipe.srtDrink}
+              {recipe.strMeal || recipe.strDrink}
             </h1>
             <img
               src={ recipe.strMealThumb || recipe.strDrinkThumb }
@@ -56,6 +83,8 @@ function RecipeDetails({ match }) {
                       data-testid={ `${index}-ingredient-name-and-measure` }
                     >
                       {ing}
+                      {' '}
+                      {pound[index]}
                     </li>
                   ))
                 }
@@ -63,20 +92,50 @@ function RecipeDetails({ match }) {
             </div>
             <p data-testid="instructions">
               {recipe.strInstructions}
-              {' '}
-
             </p>
-            <iframe
-
-              data-testid="video"
-              src={ recipe.strYoutube.replace('watch?v=', 'embed/') }
-              title="video youtube"
-              allowFullScreen
-              frameBorder="0"
-            />
+            { type === 'meals' && (
+              <iframe
+                data-testid="video"
+                src={ recipe.strYoutube.replace('watch?v=', 'embed/') }
+                title="video youtube"
+                allowFullScreen
+                frameBorder="0"
+              />
+            )}
+            { type === 'drinks' && (
+              <p data-testid="recipe-category">{recipe.strAlcoholic}</p>
+            )}
           </div>
         ))
       }
+      <button
+        data-testid="start-recipe-btn"
+        type="button"
+        style={ { position: 'fixed', bottom: '0' } }
+        onClick={ () => history.push(`/${type}/${id}/in-progress`) }
+      >
+        Start Recipe
+      </button>
+      <button
+        data-testid="share-btn"
+        type="button"
+        style={ { marginLeft: '200px' } }
+        onClick={ () => {
+          copy(`http://localhost:3000${location.pathname}`);
+          setShowCopy(true);
+        } }
+      >
+        Share
+      </button>
+      { showCopy && <p>Link copied!</p>}
+      <button
+        data-testid="favorite-btn"
+        type="button"
+        style={ { marginLeft: '200px' } }
+        onClick={ () => saveFavorites(detailsRecipes[type][0]) }
+      >
+        Favorite
+      </button>
     </div>
   );
 }
@@ -84,6 +143,7 @@ function RecipeDetails({ match }) {
 RecipeDetails.propTypes = {
   match: PropTypes.shape({ path: PropTypes.string,
     params: PropTypes.shape({ id: PropTypes.string }) }).isRequired,
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
 };
 
 export default RecipeDetails;
