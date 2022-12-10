855876105a2da7103ae928e99df84bf970a37b7a
RaphaelRighetti <raphaelrighetti@hotmail.com>
2022-12-10 17:33:42 -0300

    req 44, 45, 46, 47, 48, 49, done :rocket:

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import FiltersBtns from '../components/FiltersBtns';
 import Header from '../components/Header';
+import RecipeCard from '../components/RecipeCard';
+import { getFromLocal } from '../services/storage';
 
 export default function DoneRecipes() {
+  const [favRecipes, setFavRecipes] = useState([]);
+  const [filterParam, setFilterParam] = useState('');
+  useEffect(() => {
+    const local = typeof getFromLocal('doneRecipes') === 'string' ? []
+      : getFromLocal('doneRecipes');
+    setFavRecipes(local);
+  }, []);
   return (
     <div>
       <header>
         <Header pageTitle="Done Recipes" searchBtn={ false } />
-
       </header>
-
+      <FiltersBtns setFilterParam={ setFilterParam } />
+      {favRecipes.filter((e) => e.type.includes(filterParam)).map((e, index) => (
+        <RecipeCard
+          key={ `${e.name}-${e.type}-${e.id}` }
+          recipe={ e }
+          index={ index }
+          setFavRecipes={ setFavRecipes }
+          isDonePage
+        />
+      ))}
     </div>
   );
 }
